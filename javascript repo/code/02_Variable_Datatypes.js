9955da257596e2c4683d3f41f937f859913bef2c
Mukhia <akash-kishore.mukhia@capgemini.com>
2025-03-11 16:02:44 +0530

    remove comments from @02_Variable_Datatypes.js file

--- javascript repo/code/02_Variable_Datatypes.js
+++ javascript repo/code/02_Variable_Datatypes.js
@@ -2,23 +2,23 @@
 // `var`, `let`, `const`.
 
 // `Var` Key has a function scoped or globally-scoped behaviour.
-// var key = 0;
-// var key = 14;   // redeclaration is allowed
-// key = 'fourteen'    // reassignment is allowed
-// console.log(key);
+var key = 0;
+var key = 14;   // redeclaration is allowed
+key = 'fourteen'    // reassignment is allowed
+console.log(key);
 
 
 // `let` key has a blcok-scoped
-// let key = 0;
-// let key = 14;   // redeclaration is not allowed
-// key = 'fourteen'    // reassingment is allowed
-// console.log(key);
+let key = 0;
+let key = 14;   // redeclaration is not allowed
+key = 'fourteen'    // reassingment is allowed
+console.log(key);
 
 // `const` key has a block-scoped
-// const key = 0;
-// const key = 14;     // redeclaration is not allowed
-// key = 'fourteen'     // reassingment is not allowed
-// console.log(key);
+const key = 0;
+const key = 14;     // redeclaration is not allowed
+key = 'fourteen'     // reassingment is not allowed
+console.log(key);
 
 
 // Data-type 
@@ -27,17 +27,17 @@
     // 2. String
     // 3. Boolean
     // 4. Undefined (a variable that has been declared but not assigned any value)
-        // let und;
-        // console.log(und);
+        let und;
+        console.log(und);
     
     // 5. Null (Represents a intentional absence of value)
-        // let empty = null;
-        // console.log(empty);
+        let empty = null;
+        console.log(empty);
     
     // 6. Symbol
     // 7. BigInt (Represent number larger than Number.MAX_SAFE_INTEGER)
-        // let num = 9007199254740994n
-        // console.log(typeof(num))
+        let num = 9007199254740994n
+        console.log(typeof(num))
         
 
 // non-primitive datatype
