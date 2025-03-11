e4f04f5f448d79d9dd84a9bdec3a220cda3aced6
Mukhia <akash-kishore.mukhia@capgemini.com>
2025-03-11 15:59:58 +0530

    added @code folder javascript code files

---
+++ javascript repo/code/01_Introduction.js
@@ -0,0 +1,24 @@
+// loogging information in console
+console.log('Hello World');
+
+// comments in code:
+    // single-line comment -> //
+    // multiline comment -> /* */
+
+// key features of javascript:
+    // Client-side scripting.
+    // Versetile
+    // Event driven
+    // Asynchronous
+    // Rich Eco-system
+
+
+// Just-In-Time (JIT) compilation is a technique used by JavaScript engines (like V8) to improve performance. 
+// Here’s how it works
+    // Interpretation: Initially, the code is interpreted line-by-line by the engine.
+    // Hot Code Detection: The engine identifies frequently executed code, such as often-called functions.
+    // Compilation: The “hot” code is compiled into optimized machine code for faster execution.
+    // Execution: The compiled machine code is then executed directly, improving performance compared to repeated interpretation.
+    // JIT compilation balances between interpretation (for quick startup) and compilation (for faster execution).
+
+
---
+++ javascript repo/code/02_Variable_Datatypes.js
@@ -0,0 +1,49 @@
+// In javascript varibale are declared using following keywords
+// `var`, `let`, `const`.
+
+// `Var` Key has a function scoped or globally-scoped behaviour.
+// var key = 0;
+// var key = 14;   // redeclaration is allowed
+// key = 'fourteen'    // reassignment is allowed
+// console.log(key);
+
+
+// `let` key has a blcok-scoped
+// let key = 0;
+// let key = 14;   // redeclaration is not allowed
+// key = 'fourteen'    // reassingment is allowed
+// console.log(key);
+
+// `const` key has a block-scoped
+// const key = 0;
+// const key = 14;     // redeclaration is not allowed
+// key = 'fourteen'     // reassingment is not allowed
+// console.log(key);
+
+
+// Data-type 
+// primitive datatype
+    // 1. Number
+    // 2. String
+    // 3. Boolean
+    // 4. Undefined (a variable that has been declared but not assigned any value)
+        // let und;
+        // console.log(und);
+    
+    // 5. Null (Represents a intentional absence of value)
+        // let empty = null;
+        // console.log(empty);
+    
+    // 6. Symbol
+    // 7. BigInt (Represent number larger than Number.MAX_SAFE_INTEGER)
+        // let num = 9007199254740994n
+        // console.log(typeof(num))
+        
+
+// non-primitive datatype
+    // 1. Object (Represnts key-value pair)
+    // 2. Array (Represents an ordered list of values)
+    // 3. Function (Represents reusable block of code)
+
+
+
